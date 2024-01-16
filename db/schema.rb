941eb1cb78e7b9270429c9b26f04bd3c71829ae6
fatima mitir <mitirfatima22@gmail.com>
2024-01-16 23:58:31 +0100

    bb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_16_134328) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_16_204543) do
   create_table "bookings", force: :cascade do |t|
     t.date "starts_at"
     t.date "ends_at"
@@ -43,6 +43,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_134328) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "rooms"
